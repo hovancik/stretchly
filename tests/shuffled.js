5d1a4da4a9328bd725562051c47e06d314cf0b98
Jan Hovancik <conta.srdr@gmail.com>
2016-08-27 17:31:07 +0200

    Fixes linter stuff

--- tests/shuffled.js
+++ tests/shuffled.js
@@ -1,21 +1,20 @@
 'use strict'
 
 var chai = require('chai')
-var path = require('path')
-var Shuffled =  require('../app/shuffled')
+var Shuffled = require('../app/shuffled')
 
 chai.should()
 
 describe('shuffled', function () {
   beforeEach(function () {
-    this.microbreakIdeas = new Shuffled([1,2,3,4])
+    this.microbreakIdeas = new Shuffled([1, 2, 3, 4])
   })
 
   it('returns random elements without repetition', function () {
     let sum = 0
     for (let i = 1; i < 20000; i++) {
       sum += this.microbreakIdeas.randomElement()
-      if (i % 4 === 0 ) {
+      if (i % 4 === 0) {
         sum.should.equal(10)
         sum = 0
       }
