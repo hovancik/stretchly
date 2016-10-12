6c64a604f736fbc2c37b58b6ba6ee9f01c16e188
Jan Hovancik <conta.srdr@gmail.com>
2016-10-12 18:12:17 +0200

    Release version

--- app/process.js
+++ app/process.js
@@ -1,6 +1,28 @@
-const {ipcRenderer} = require('electron')
+const {ipcRenderer, shell, remote} = require('electron')
+let VersionChecker = require('./utils/versionChecker')
 
 ipcRenderer.on('playSound', (event, data) => {
   let audio = new Audio(`audio/${data}.wav`)
   audio.play()
 })
+
+ipcRenderer.on('checkVersion', (event, data) => {
+  if (remote.getGlobal('shared').isNewVersion) {
+    notifyNewVersion()
+  } else {
+    new VersionChecker().latest(function (version) {
+      if (data !== version) {
+        remote.getGlobal('shared').isNewVersion = true
+        ipcRenderer.send('update-tray')
+        notifyNewVersion()
+      }
+    })
+  }
+})
+
+function notifyNewVersion () {
+  let notification = new Notification('stretchly', {
+    body: 'New version is available!'
+  })
+  notification.onclick = () => shell.openExternal('https://github.com/hovancik/stretchly/releases')
+}
