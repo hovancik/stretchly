c113afd2ab1f5bb03e90c36dee0db8c7f079b904
Sean Manton <sean@seanxmanton.com>
2017-04-08 16:55:46 -0400

    Adds info to tooltip on time till next break or end of pause

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
+### Added
+- Time till next break/breaks resume if paused shown in tooltip
+
 ### Fixed
 - version checker erroneous 'a new version is available' messages
 
--- app/utils/utils.js
+++ app/utils/utils.js
@@ -13,5 +13,39 @@ let updateProgress = function (started, duration, progress, progressTime) {
   }
 }
 
+let formatTillBreak = function(milliseconds) {
+  let minutes = Math.round(milliseconds / 60000);
+  if (minutes < 1) {
+    let seconds = ((milliseconds % 60000) / 1000).toFixed(0)
+    return `${seconds}s`
+  } else {
+    return `${minutes}m`
+  }
+}
+
+let formatPauseTimeLeft = function (milliseconds) {
+  let timeString = ''
+  let hours = Math.floor(milliseconds / (1000 * 3600))
+  let remainder = (milliseconds - hours * 1000 * 3600)
+  let minutes = Math.floor(remainder / 60000)
+  if (minutes >= 60) {
+    minutes -= 60
+    hours += 1
+  }
+  if (hours >= 1) {
+    timeString += `${hours}h`
+  }
+  if (minutes >= 1) {
+    timeString += `${minutes}m`
+  }
+  if (minutes < 1 && hours < 1) {
+    timeString = 'less than 1m'
+  }
+  return timeString
+}
+
+
 module.exports.formatRemaining = formatRemaining
+module.exports.formatTillBreak = formatTillBreak
+module.exports.formatPauseTimeLeft = formatPauseTimeLeft
 module.exports.updateProgress = updateProgress
