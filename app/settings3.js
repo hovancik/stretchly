beef27b701c599bb12de04cf406faf2efb9be36d
Jan Hovancik <conta.srdr@gmail.com>
2017-08-28 17:13:28 +0200

    Adds option to not show break ideas
    closes #130

--- app/settings3.js
+++ app/settings3.js
@@ -24,5 +24,5 @@ ipcRenderer.on('renderSettings', (event, data) => {
 })
 
 document.getElementById('defaults').addEventListener('click', function (e) {
-  ipcRenderer.send('set-default-settings', ['fullscreen'])
+  ipcRenderer.send('set-default-settings', ['fullscreen', 'ideas'])
 })
