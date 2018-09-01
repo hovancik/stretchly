955f724346b37fb9761b4b65f139662e3631004b
Alina Leuca <alinaleuca@gmail.com>
2018-09-01 11:01:19 +0200

    update schema as example

--- app/database/schema.js
+++ app/database/schema.js
@@ -1,10 +1,15 @@
-let sqlite = require('sqlite3').verbose()
-let db = new sqlite.Database('stretchly.sqlite')
+const sqlite = require('sqlite3').verbose()
+const db = new sqlite.Database('stretchly.sqlite', (err) => {
+    if(err) {
+        console.error(err.message)
+    }
+})
 
 db.serialize(() => {
-    db.run("CREATE TABLE breaks(id INT, name TEXT)")
+    db.run('CREATE TABLE breaks(total_count INT, total_time INT)')
+        .run('INSERT INTO breaks()')
 
-    let test = db.prepare("INSERT INTO breaks VALUES (1, 'hi')")
+    const test = db.prepare("INSERT INTO breaks VALUES (1, 'hi')")
     test.run()
     test.finalize()
 
@@ -14,3 +19,25 @@ db.serialize(() => {
 
     db.close()
 })
+
+// db.serialize(() => {
+//     // Queries scheduled here will be serialized.
+//     db.run('CREATE TABLE greetings(message text)')
+//       .run(`INSERT INTO greetings(message)
+//             VALUES('Hi'),
+//                   ('Hello'),
+//                   ('Welcome')`)
+//       .each(`SELECT message FROM greetings`, (err, row) => {
+//         if (err){
+//           throw err;
+//         }
+//         console.log(row.message);
+//       });
+//   });
+   
+//   // close the database connection
+//   db.close((err) => {
+//     if (err) {
+//       return console.error(err.message);
+//     }
+//   });
