9b19a154d393594755a8878cdc3ae875b8ba8d63
Jan Hovancik <conta.srdr@gmail.com>
2018-12-27 16:20:02 +0100

    Adds more contributor settings

--- app/contributor-settings.js
+++ app/contributor-settings.js
@@ -8,6 +8,20 @@ let microbreakNotificationInterval = document.getElementById('microbreakNotifica
 let breakNotificationIntervalPlus = document.getElementById('breakNotificationIntervalPlus')
 let breakNotificationIntervalMinus = document.getElementById('breakNotificationIntervalMinus')
 let breakNotificationInterval = document.getElementById('breakNotificationInterval')
+let breakPostponeTimePlus = document.getElementById('breakPostponeTimePlus')
+let breakPostponeTimeMinus = document.getElementById('breakPostponeTimeMinus')
+let breakPostponeTime = document.getElementById('breakPostponeTime')
+let microbreakPostponeTimePlus = document.getElementById('microbreakPostponeTimePlus')
+let microbreakPostponeTimeMinus = document.getElementById('microbreakPostponeTimeMinus')
+let microbreakPostponeTime = document.getElementById('microbreakPostponeTime')
+let breakPostponableDurationPercentPlus = document.getElementById('breakPostponableDurationPercentPlus')
+let breakPostponableDurationPercentMinus = document.getElementById('breakPostponableDurationPercentMinus')
+let breakPostponableDurationPercent = document.getElementById('breakPostponableDurationPercent')
+let microbreakPostponableDurationPercentPlus = document.getElementById('microbreakPostponableDurationPercentPlus')
+let microbreakPostponableDurationPercentMinus = document.getElementById('microbreakPostponableDurationPercentMinus')
+let microbreakPostponableDurationPercent = document.getElementById('microbreakPostponableDurationPercent')
+
+
 
 document.addEventListener('dragover', event => event.preventDefault())
 document.addEventListener('drop', event => event.preventDefault())
@@ -36,9 +50,73 @@ breakNotificationIntervalMinus.addEventListener('click', function (e) {
   }
 })
 
+breakPostponeTimePlus.addEventListener('click', function (e) {
+  if (breakNotificationInterval.innerHTML !== '600') {
+    ipcRenderer.send('save-setting', 'breakPostponeTime', (parseInt(breakPostponeTime.innerHTML, 10) + 5) * 1000)
+  }
+})
+
+breakPostponeTimeMinus.addEventListener('click', function (e) {
+  if (breakPostponeTime.innerHTML !== '60') {
+    ipcRenderer.send('save-setting', 'breakPostponeTime', (parseInt(breakPostponeTime.innerHTML, 10) - 5) * 1000)
+  }
+})
+
+microbreakPostponeTimePlus.addEventListener('click', function (e) {
+  if (breakNotificationInterval.innerHTML !== '600') {
+    ipcRenderer.send('save-setting', 'microbreakPostponeTime', (parseInt(microbreakPostponeTime.innerHTML, 10) + 5) * 1000)
+  }
+})
+
+microbreakPostponeTimeMinus.addEventListener('click', function (e) {
+  if (microbreakPostponeTime.innerHTML !== '60') {
+    ipcRenderer.send('save-setting', 'microbreakPostponeTime', (parseInt(microbreakPostponeTime.innerHTML, 10) - 5) * 1000)
+  }
+})
+
+breakPostponableDurationPercentPlus.addEventListener('click', function (e) {
+  console.log(breakNotificationInterval.innerHTML)
+  if (breakPostponableDurationPercentPlus.innerHTML !== '80') {
+    ipcRenderer.send('save-setting', 'breakPostponableDurationPercent', (parseInt(breakPostponableDurationPercent.innerHTML, 10) + 5))
+  }
+})
+
+breakPostponableDurationPercentMinus.addEventListener('click', function (e) {
+  if (breakPostponableDurationPercent.innerHTML !== '5') {
+    ipcRenderer.send('save-setting', 'breakPostponableDurationPercent', (parseInt(breakPostponableDurationPercent.innerHTML, 10) - 5))
+  }
+})
+
+microbreakPostponableDurationPercentPlus.addEventListener('click', function (e) {
+  if (microbreakPostponableDurationPercent.innerHTML !== '80') {
+    ipcRenderer.send('save-setting', 'microbreakPostponableDurationPercent', (parseInt(microbreakPostponableDurationPercent.innerHTML, 10) + 5))
+  }
+})
+
+microbreakPostponableDurationPercentMinus.addEventListener('click', function (e) {
+  if (microbreakPostponableDurationPercent.innerHTML !== '5') {
+    ipcRenderer.send('save-setting', 'microbreakPostponableDurationPercent', (parseInt(microbreakPostponableDurationPercent.innerHTML, 10) - 5))
+  }
+})
+
 ipcRenderer.on('renderSettings', (event, data) => {
+  let enableElements = document.getElementsByClassName('enable')
+  for (let i = 0; i < enableElements.length; i++) {
+    let element = enableElements[i]
+    element.checked = data[element.value]
+    if (!eventsAttached) {
+      element.addEventListener('click', function (e) {
+        ipcRenderer.send('save-setting', element.value, element.checked)
+      })
+    }
+  }
+
   microbreakNotificationInterval.innerHTML = data['microbreakNotificationInterval'] / 1000
   breakNotificationInterval.innerHTML = data['breakNotificationInterval'] / 1000
+  breakPostponeTime.innerHTML = data['breakPostponeTime'] / 1000
+  microbreakPostponeTime.innerHTML = data['microbreakPostponeTime'] / 1000
+  breakPostponableDurationPercent.innerHTML = data['breakPostponableDurationPercent']
+  microbreakPostponableDurationPercent.innerHTML = data['microbreakPostponableDurationPercent']
   document.body.style.background = data['mainColor']
   eventsAttached = true
 })
--- app/main.js
+++ app/main.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow, Tray, Menu, ipcMain, shell, dialog, globalShortcut }
 const path = require('path')
 const i18next = require('i18next')
 const Backend = require('i18next-node-fs-backend')
-const path = require('path')
+const notificationState = require('@meetfranz/electron-notification-state')
 
 startI18next()
 
