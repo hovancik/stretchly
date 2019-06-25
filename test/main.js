3b5c9f0c714b6f031e13a8293419e9b52e53e5e1
Jan Hovancik <conta.srdr@gmail.com>
2019-06-25 13:15:03 +0200

    Use rimraf to fix Win tests
    scustom sync removal of files was causing errors in tests

--- test/main.js
+++ test/main.js
@@ -2,33 +2,13 @@ let Application = require('spectron').Application
 let chai = require('chai')
 let chaiAsPromised = require('chai-as-promised')
 let electronPath = require('electron')
-const fs = require('fs')
-const path = require('path')
+const rimraf = require('rimraf')
 
 const { modifySettings } = require('./modifySettingsHelper')
 
 chai.should()
 chai.use(chaiAsPromised)
 const timeout = process.env.CI ? 60000 : 10000
-
-/**
- * Remove directory recursively
- * @param {string} dir_path
- * @see https://stackoverflow.com/a/42505874/3027390
- */
-function rimraf(dir_path) {
-  if (fs.existsSync(dir_path)) {
-    fs.readdirSync(dir_path).forEach(function(entry) {
-      let entry_path = path.join(dir_path, entry)
-      if (fs.lstatSync(entry_path).isDirectory()) {
-        rimraf(entry_path)
-      } else {
-        fs.unlinkSync(entry_path)
-      }
-    })
-    fs.rmdirSync(dir_path)
-  }
-}
 
 describe('stretchly', function () {
   this.timeout(timeout)
@@ -38,7 +18,7 @@ describe('stretchly', function () {
 
   afterEach(function () {
     if (this.app && this.app.isRunning()) {
-      rimraf(`${__dirname}/stretchly-test-tmp`)
+      rimraf(`${__dirname}/stretchly-test-tmp`, () => {})
       return this.app.stop()
     }
   })
