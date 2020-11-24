3884f99ea339239a3bf9bd36bb3c03aa6c5a7cb2
mwoz123 <mwoz123@github.com>
2020-11-24 21:26:45 +0100

    black on white theme

--- app/main.js
+++ app/main.js
@@ -443,7 +443,7 @@ function startMicrobreak () {
       }
       microbreakWinLocal.webContents.send('microbreakIdea', idea)
       microbreakWinLocal.webContents.send('progress', startTime,
-        breakDuration, strictMode, postponable, postponableDurationPercent, settings.get('endBreakShortcut'))
+        breakDuration, strictMode, postponable, postponableDurationPercent, settings)
       if (!settings.get('fullscreen') && process.platform !== 'darwin') {
         setTimeout(() => {
           microbreakWinLocal.center()
@@ -553,7 +553,7 @@ function startBreak () {
       }
       breakWinLocal.webContents.send('breakIdea', idea)
       breakWinLocal.webContents.send('progress', startTime,
-        breakDuration, strictMode, postponable, postponableDurationPercent, settings.get('endBreakShortcut'))
+        breakDuration, strictMode, postponable, postponableDurationPercent, settings)
       if (!settings.get('fullscreen') && process.platform !== 'darwin') {
         setTimeout(() => {
           breakWinLocal.center()
