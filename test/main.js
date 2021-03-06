bac239c7a161b2d7dd64011cc1d958dc9d3ae860
ilmars <9987548+darksworm@users.noreply.github.com>
2021-03-06 14:39:19 +0200

    basic e2e tests

--- test/main.js
+++ test/main.js
@@ -1,17 +1,28 @@
 const Application = require('spectron').Application
+const electronPath = require('electron')
+const AppSettings = require('../app/utils/settings')
+
 const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
-const electronPath = require('electron')
-const rimraf = require('rimraf')
 
-const { modifySettings } = require('./modifySettingsHelper')
+const path = require('path')
+const rimraf = require('rimraf')
 
 chai.should()
 chai.use(chaiAsPromised)
 const timeout = process.env.CI ? 60000 : 10000
 
 describe('stretchly', function () {
+  const tempDir = `${__dirname}/stretchly-test-tmp`
+  const settingsFile = `${path.join(tempDir, 'config.json')}`
+  const settings = new AppSettings(settingsFile)
+
   this.timeout(timeout)
+
+  async function launchAndWaitForWindowToLoad(app) {
+    await app.start()
+    await app.client.waitUntilWindowLoaded()
+  }
 
   beforeEach(function () {
     this.app = new Application({
@@ -20,102 +31,113 @@ describe('stretchly', function () {
         `${__dirname}/../app`
       ],
       chromeDriverArgs: [
-        `--user-data-dir=${__dirname}/stretchly-test-tmp`
+        `--user-data-dir=${tempDir}`
       ]
     })
-
-    return modifySettings('isFirstRun', false)
   })
 
   afterEach(function () {
+    this.app.client.getMainProcessLogs().then(function (logs) {
+      logs.forEach((log) => {
+        log.split('\n')
+          .forEach((l) => console.log('[APP]\t' + l))
+      })
+    })
+
+    // using sync rimraf because async causes flaky tests even if awaited
+    rimraf.sync(tempDir)
+
     if (this.app && this.app.isRunning()) {
-      rimraf(`${__dirname}/stretchly-test-tmp`, () => {})
       return this.app.stop()
     }
   })
 
-  it('main process runs on app start', function () {
-    return this.app.start()
-      .then(async () => {
-        chaiAsPromised.transferPromiseness = this.app.transferPromiseness
+  describe('on first run', function () {
+    beforeEach(async function () {
+      settings.set('isFirstRun', true)
+      await launchAndWaitForWindowToLoad(this.app)
+    })
 
-        await this.app.client
-          .waitUntilWindowLoaded()
+    it('creates two windows', async function () {
+      let windowCount = await this.app.client.getWindowCount()
+      windowCount.should.equal(2)
+    })
 
-        let windowCount = await this.app.client.getWindowCount()
+    it('first window is visible', async function () {
+      let visible = await this.app.client.browserWindow.isVisible()
+      visible.should.equal(true)
+    })
 
-        windowCount.should.be.at.least(2)
-      })
+    it('main window is the welcome window', async function () {
+      let title = await this.app.client.browserWindow.getTitle()
+      title.should.contain('Welcome')
+    })
+
+    it('second window is not visible', async function () {
+      await this.app.client.windowByIndex(1)
+      let visible = await this.app.client.browserWindow.isVisible()
+      visible.should.equal(false)
+    })
+
   })
 
-  it('welcome window opens on first run', function () {
-    return modifySettings('isFirstRun', true)
-      .then(() => {
-        return this.app.start()
-          .then(async () => {
-            chaiAsPromised.transferPromiseness = this.app.transferPromiseness
+  describe('on subsequent runs', function () {
+    beforeEach(async function () {
+      settings.set('isFirstRun', false)
+      await launchAndWaitForWindowToLoad(this.app)
+    })
 
-            await this.app.client
-                  .waitUntilWindowLoaded()
+    it('creates only one window', async function () {
+      let windowCount = await this.app.client.getWindowCount()
+      windowCount.should.equal(1)
+    })
 
-            let windowCount = await this.app.client.getWindowCount()
+    it('created window is not visible', async function () {
+      let visible = await this.app.client.browserWindow.isVisible()
+      visible.should.equal(false)
+    })
 
-            windowCount.should.equal(2)
+    it('main window is not the welcome window', async function () {
+      let title = await this.app.client.browserWindow.getTitle()
+      title.should.not.contain('Welcome')
+    })
 
-            await this.app.client.windowByIndex(0)
-            let visible = await this.app.client.browserWindow.isVisible()
-
-            visible.should.equal(true)
-
-            await this.app.client.windowByIndex(1)
-            visible = await this.app.client.browserWindow.isVisible()
-
-            visible.should.equal(false)
-          })
-      })
+    it('main window is the Stretchly window', async function () {
+      let title = await this.app.client.browserWindow.getTitle()
+      title.should.equal('Stretchly')
+    })
   })
 
-  it('welcome window does not open after first run', function () {
-    return modifySettings('isFirstRun', false)
-      .then(() => {
-        return this.app.start()
-          .then(async () => {
-            chaiAsPromised.transferPromiseness = this.app.transferPromiseness
-            await this.app.client
-              .waitUntilWindowLoaded()
+  describe('microbreak window', function () {
+    beforeEach('force microbreak', async function () {
+      settings.set('isFirstRun', false)
 
-            let windowCount = await this.app.client.getWindowCount()
-            windowCount.should.equal(1)
+      settings.set('microbreak', true)
+      settings.set('microbreakInterval', 200)
+      settings.set('microbreakNotificationInterval', 100)
 
-            await this.app.client.windowByIndex(0)
+      await launchAndWaitForWindowToLoad(this.app)
 
-            let visible = await this.app.client.browserWindow.isVisible()
-            visible.should.equal(true)
-          })
-      })
+      // give the microbreak a moment to be created and displayed
+      await new Promise(resolve => setTimeout(resolve, 250))
+
+      // select the microbreak window
+      await this.app.client.windowByIndex(1)
+    })
+
+    it('is created', async function () {
+      let title = await this.app.client.browserWindow.getTitle()
+      title.should.contain('take a break')
+    })
+
+    it('cannot be closed', async function() {
+      await this.app.client.browserWindow.close()
+      let isDestroyed = await this.app.client.browserWindow.isDestroyed()
+      isDestroyed.should.equal(false)
+    })
+
+    it('has postpone button', async function() {
+      await this.app.client.waitUntilTextExists('#postpone', 'Postpone', 500)
+    })
   })
-
-//  it('main window is stretchly ', function () {
-//    return modifySettings('isFirstRun', false)
-//      .then(() => {
-//        this.app = new Application({
-//          path: electronPath,
-//          args: [
-//            `${__dirname}/../app`
-//          ],
-//          chromeDriverArgs: [
-//            `--user-data-dir=${__dirname}/stretchly-test-tmp`
-//          ]
-//        })
-//
-//        return this.app.start()
-//          .then(() => {
-//            chaiAsPromised.transferPromiseness = this.app.transferPromiseness
-//            return this.app.client
-//              .waitUntilWindowLoaded()
-//              .windowByIndex(0).browserWindow
-//              .getTitle().should.eventually.equal('Stretchly')
-//          })
-//      })
-//  })
 })
