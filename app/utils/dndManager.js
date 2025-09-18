46d22a57db1f671dddc9411c8f914038479e778b
Jan Hovancik <jan@hovancik.net>
2025-09-18 15:56:10 +0200

    Refactor DndManager to ensure session bus disconnects properly and optimize asyncExec initialization

--- app/utils/dndManager.js
+++ app/utils/dndManager.js
@@ -36,6 +36,10 @@ class DndManager extends EventEmitter {
     this.isOnDnd = false
     clearTimeout(this.timer)
     this.timer = null
+    if (this.__sessionBus) {
+      this.__sessionBus.disconnect()
+      this.__sessionBus = null
+    }
     log.info('Stretchly: stopping Do Not Disturb monitoring')
   }
 
@@ -49,7 +53,7 @@ class DndManager extends EventEmitter {
 
   async _isDndEnabledLinux () {
     const de = this._desktopEnviroment.toLowerCase()
-    const sessionBus = dbus.sessionBus()
+    const sessionBus = this.__sessionBus ||= dbus.sessionBus()
     switch (true) {
       case de.includes('kde'):
         try {
@@ -77,7 +81,7 @@ class DndManager extends EventEmitter {
         break
       case de.includes('gnome') || de.includes('unity'):
         try {
-          const asyncExec = promisify(exec)
+          const asyncExec = this.__asyncExec ||= promisify(exec)
           const { stdout } = await asyncExec('gsettings get org.gnome.desktop.notifications show-banners')
           if (stdout.replace(/[^0-9a-zA-Z]/g, '') === 'false') {
             return true
@@ -88,7 +92,7 @@ class DndManager extends EventEmitter {
         break
       case de.includes('cinnamon'):
         try {
-          const asyncExec = promisify(exec)
+          const asyncExec = this.__asyncExec ||= promisify(exec)
           const { stdout } = await asyncExec('gsettings get org.cinnamon.desktop.notifications display-notifications')
           if (stdout.replace(/[^0-9a-zA-Z]/g, '') === 'false') {
             return true
@@ -99,7 +103,7 @@ class DndManager extends EventEmitter {
         break
       case de.includes('mate'):
         try {
-          const asyncExec = promisify(exec)
+          const asyncExec = this.__asyncExec ||= promisify(exec)
           const { stdout } = await asyncExec('gsettings get org.mate.NotificationDaemon do-not-disturb')
           if (stdout.replace(/[^0-9a-zA-Z]/g, '') === 'true') {
             return true
@@ -130,7 +134,7 @@ class DndManager extends EventEmitter {
         return wfa !== -1 && wfa !== 0
       } else if (process.platform === 'darwin') {
         try {
-          const asyncExec = promisify(exec)
+          const asyncExec = this.__asyncExec ||= promisify(exec)
           const { stdout } = await asyncExec('defaults read com.apple.controlcenter "NSStatusItem Visible FocusModes"')
           if (stdout.replace(/[^0-9a-zA-Z]/g, '') === '1') {
             return true
